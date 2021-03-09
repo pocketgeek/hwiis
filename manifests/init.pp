8132b40720e18fb904e82610c7bd8db69e680984
Curtis Edge <pocket_geek@pgnet.us>
2021-03-09 13:50:26 -0700

    things

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include hwiis
+class hwiis {
+
+  windowsfeature { 'Web-WebServer':
+    ensure                 => present,
+    installmanagementtools => true,
+    installsubfeatures => true,
+  }
+
+}
