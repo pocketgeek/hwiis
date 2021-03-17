49b4c11b673296c6028b190d8d7c305d5f46e7ce
Curtis Edge <pocket_geek@pgnet.us>
2021-03-17 13:59:05 -0600

    fun with hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,33 +4,36 @@
 #
 # @example
 #   include hwiis
-class hwiis {
+class hwiis (
+  $websitename,
+  $websitedirectory,
+) {
 
-$iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
 
-iis_feature { $iis_features:
-  ensure => 'present',
-}
+  iis_feature { $iis_features:
+    ensure => 'present',
+  }
 
-# Delete the default website to prevent a port binding conflict.
-iis_site {'Default Web Site':
-  ensure  => absent,
-  require => Iis_feature['Web-WebServer'],
-}
+  # Delete the default website to prevent a port binding conflict.
+  # And because it's terrible and should feel bad.
+  iis_site {'Default Web Site':
+    ensure  => absent,
+    require => Iis_feature['Web-WebServer'],
+  }
 
-iis_site { 'minimal':
-  ensure          => 'started',
-  physicalpath    => 'c:\\inetpub\\minimal',
-  applicationpool => 'DefaultAppPool',
-  require         => [
-    File['minimal'],
-    Iis_site['Default Web Site']
-  ],
-}
+  iis_site { $websitename:
+    ensure          => 'started',
+    physicalpath    => $websitedirectory,
+    applicationpool => 'DefaultAppPool',
+    require         => [
+      File[ $websitedirectory ],
+      Iis_site['Default Web Site']
+    ],
+  }
 
-file { 'minimal':
-  ensure => 'directory',
-  path   => 'c:\\inetpub\\minimal',
-}
+  file { $websitedirectory:
+    ensure => 'directory',
+  }
 
 }
